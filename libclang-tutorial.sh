df0076c60ec4e39d45e4d97638ae55e1abfc5fc9
Simon Rong <75641024+Simon-23@users.noreply.github.com>
2024-08-03 01:42:26 -0400

    Create libclang-tutorial.sh

---
+++ libclang-tutorial.sh
@@ -0,0 +1,16 @@
+          echo 1. cloning tiagos libclang tutorial
+          git clone https://github.com/cogumbreiro/libclang-tutorial
+          echo
+          echo 2. Go inside the repo directory
+          cd libclang-tutorial/
+          echo
+          echo 3. export path
+          export "PATH=$HOMEBREW_PREFIX/opt/llvm@15/bin:$PATH"
+          echo 4. Cleaning up make
+          make clean
+          echo
+          echo 5. run make
+          make
+          echo
+          echo 6. Who is running this script?
+          whoami
